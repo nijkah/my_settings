5bdf3380ecb98bbf09bad7c2caef6bc3a4c86a6f
nijkah <nijkah@gmail.com>
2022-01-28 05:59:05 +0000

    Update fast-syntax-highlighting

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -2,7 +2,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 
 # install omz plugins
-git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 chmod 755 "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
